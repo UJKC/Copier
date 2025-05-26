bd34c4530c58d593c0587e10b270bf65c09e7054
Ujwal K C <ujwalkcsps@gmail.com>
2025-05-26 22:58:44 +0530

    Bye :)

--- obj/Release/net8.0/win-x64/Copier.AssemblyInfo.cs
+++ obj/Release/net8.0/win-x64/Copier.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Copier")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96e86e2710e33abb55f57da35a89d14f5d1f1963")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+574f336273131088d7d07b79e37326acbf49c9fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Copier")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Copier")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

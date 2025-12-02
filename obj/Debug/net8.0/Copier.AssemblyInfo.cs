1988467581af720bb41f2cbc8661b57c7a5dc768
UJKC <ujwalkcsps@gmail.com>
2025-12-02 23:57:21 +0530

    Transparent Background working

--- obj/Debug/net8.0/Copier.AssemblyInfo.cs
+++ obj/Debug/net8.0/Copier.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Copier")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3eba8dd7174726f2e5c3075c62debaee864673db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b67a27dafb4ff6ebae13c53e1d26edd0a132dcf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Copier")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Copier")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
